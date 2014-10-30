32386da1fce5170888076ac25c5ca5b552241eab
David Miller <mithegooie@gmail.com>
2014-10-30 08:01:42 -0700

    added badges endpoint, jedi theme to browsable api

--- django_ecommerce/main/json_views.py
+++ django_ecommerce/main/json_views.py
@@ -1,8 +1,20 @@
 from rest_framework import mixins, generics, permissions
-from main.serializers import StatusReportSerializer
-from main.models import StatusReport
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from main.serializers import StatusReportSerializer, BadgeSerializer
+from main.models import StatusReport, Badge
 from main.permissions import IsOwnerOrReadOnly
 
+
+@api_view(('GET',))
+def api_root(request):
+    return Response({
+        'status_reports': reverse(
+            'status_reports_collection', request=request),
+        'badges': reverse('badges_collection', request=request),
+    })
+    
 
 class StatusCollection(mixins.ListModelMixin,
                        mixins.CreateModelMixin,
@@ -36,3 +48,38 @@ class StatusMember(mixins.RetrieveModelMixin,
 
     def delete(self, request, *args, **kwargs):
         return self.destroy(request, *args, **kwargs)
+
+
+class BadgeCollection(mixins.ListModelMixin,
+                      mixins.CreateModelMixin,
+                      generics.GenericAPIView):
+
+    queryset = Badge.objects.all()
+    serializer_class = BadgeSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get(self, request):
+        return self.list(request)
+
+    def post(self, request):
+        return self.create(request)
+
+
+class BadgeMember(mixins.RetrieveModelMixin,
+                  mixins.UpdateModelMixin,
+                  mixins.DestroyModelMixin,
+                  generics.GenericAPIView):
+    
+    queryset = Badge.objects.all()
+    serializer_class = BadgeSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
