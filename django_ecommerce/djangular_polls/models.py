94723bcb750582302a5a96111b283768197e3dd8
David Miller <mithegooie@gmail.com>
2014-11-22 19:31:09 -0800

    added multiuser vote state functionality

--- django_ecommerce/djangular_polls/models.py
+++ django_ecommerce/djangular_polls/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import Sum
 
 # Create your models here.
 class Poll(models.Model):
@@ -6,15 +7,25 @@ class Poll(models.Model):
     publish_date = models.DateTimeField(auto_now=True)
 
     def poll_items(self):
-        return self.pollitem_set.all()
+        return self.items.all()
+
+    @property
+    def total_votes(self):
+        return self.poll_items().aggregate(Sum('votes')).get(
+            'votes__sum', 0)
 
 class PollItem(models.Model):
     poll = models.ForeignKey(Poll, related_name='items')
     name = models.CharField(max_length=30)
     text = models.CharField(max_length=300)
     votes = models.IntegerField(default=0)
-    percentage = models.DecimalField(
-        max_digits=5, decimal_places=2, default=0.0)
+    
+    @property
+    def percentage(self):
+        total = self.poll.total_votes
+        if total:
+            return self.votes / total * 100
+        return 0
 
     class Meta:
         ordering = ['-text']
--- django_ecommerce/static/js/userPollCtrl.js
+++ django_ecommerce/static/js/userPollCtrl.js
@@ -5,21 +5,44 @@ pollsApp.config(function($interpolateProvider) {
     .endSymbol(']]');
 });
 
-pollsApp.controller('UserPollCtrl', function($scope) {
-    $scope.total_votes = 0;
-    $scope.vote_data = {}
+pollsApp.controller('UserPollCtrl', function($scope, $http) {
 
-    $scope.vote = function(voteModel) {
-        if (!$scope.vote_data.hasOwnProperty(voteModel)) {
-            $scope.vote_data[voteModel] = {"votes": 0, "percent": 0};
-            $scope[voteModel] = $scope.vote_data[voteModel];
-        }
-        $scope.vote_data[voteModel]["votes"] = 
-            $scope.vote_data[voteModel]["votes"] + 1;
-        $scope.total_votes = $scope.total_votes + 1;
-        for (var key in $scope.vote_data) {
-            item = $scope.vote_data[key];
-            item["percent"] = item["votes"] / $scope.total_votes * 100;
-        }
+    // Get the Poll
+    $scope.poll = ""
+
+    $http.get('/api/v1/polls/1').
+        success(function(data) {
+            $scope.poll = data;
+        }).
+        error(function(data, status) {
+            console.log("calling /api/v1/polls/1 returned status " + status);
+        });
+
+    $scope.total_votes = 0;
+
+    $scope.vote = function(item) {
+        item.votes += 1;
+        $http.put('/api/v1/poll_items/'+item.id, item).
+            success(function(data) {
+                $http.get('/api/v1/polls/1').success(function(data) {
+                    $scope.poll = data;
+                }).
+                error(function(data, status) {
+                    console.log("calling /api/v1/polls/1 returned status " +
+                                status);
+                });
+            }).
+            error(function(data, status) {
+                console.log("calling PUT /api/v1/poll_items returned status " +
+                            status);
+            });
+    };
+
+    $scope.barcolor = function(i) {
+        colors = ['progress-bar-success', 'progress-bar-info',
+            'progress-bar-warning', 'progress-bar-danger', '']
+        idx = i % colors.length;
+        return colors[idx];
     };
 });
+
