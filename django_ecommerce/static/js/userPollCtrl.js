5b6119a94473ae01a622c1ef2f24d680e272efcd
David Miller <mithegooie@gmail.com>
2014-11-22 20:06:53 -0800

    refactored code to use angular factories

--- django_ecommerce/static/js/userPollCtrl.js
+++ django_ecommerce/static/js/userPollCtrl.js
@@ -5,38 +5,37 @@ pollsApp.config(function($interpolateProvider) {
     .endSymbol(']]');
 });
 
-pollsApp.controller('UserPollCtrl', function($scope, $http) {
+pollsApp.factory('pollFactory', function($http) {
+
+    var baseUrl = '/api/v1/';
+    var pollUrl = baseUrl + 'polls/';
+    var pollItemsUrl = baseUrl + 'poll_items/';
+
+    var pollFactory = {};
+
+    pollFactory.getPoll = function(id) {
+        return $http.get(pollUrl + id)
+    };
+
+    pollFactory.vote_for_item = function(poll_item) {
+        poll_item.votes += 1;
+        return $http.put(pollItemsUrl + poll_item.id, poll_item);
+    }
+
+    return pollFactory;
+});
+
+pollsApp.controller('UserPollCtrl', function($scope, pollFactory) {
 
     // Get the Poll
     $scope.poll = ""
+    function setPoll(promise) {
+        $scope.poll = promise.data;
+    }
 
-    $http.get('/api/v1/polls/1').
-        success(function(data) {
-            $scope.poll = data;
-        }).
-        error(function(data, status) {
-            console.log("calling /api/v1/polls/1 returned status " + status);
-        });
-
-    $scope.total_votes = 0;
-
-    $scope.vote = function(item) {
-        item.votes += 1;
-        $http.put('/api/v1/poll_items/'+item.id, item).
-            success(function(data) {
-                $http.get('/api/v1/polls/1').success(function(data) {
-                    $scope.poll = data;
-                }).
-                error(function(data, status) {
-                    console.log("calling /api/v1/polls/1 returned status " +
-                                status);
-                });
-            }).
-            error(function(data, status) {
-                console.log("calling PUT /api/v1/poll_items returned status " +
-                            status);
-            });
-    };
+    function getPoll() {
+        return pollFactory.getPoll(1);
+    }
 
     $scope.barcolor = function(i) {
         colors = ['progress-bar-success', 'progress-bar-info',
@@ -44,5 +43,13 @@ pollsApp.controller('UserPollCtrl', function($scope, $http) {
         idx = i % colors.length;
         return colors[idx];
     };
+
+    getPoll().then(setPoll);
+
+    $scope.vote = function(item) {
+        pollFactory.vote_for_item(item)
+            .then(getPoll)
+            .then(setPoll);
+    }
 });
 
